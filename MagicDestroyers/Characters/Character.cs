38c344c1323aea16ba42e320da6d06683f4428a2
nguyhai <tenth11@gmail.com>
2018-08-14 16:23:14 -0500

    Test using VS 2015

--- MagicDestroyers/Characters/Character.cs
+++ MagicDestroyers/Characters/Character.cs
@@ -21,7 +21,7 @@ namespace MagicDestroyers.Characters
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("I am moving");
-                Thread.Sleep(pauseAmount);
+                Thread.Sleep(pauseAmount); 
             }
         }
 
